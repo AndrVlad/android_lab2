b0fdcd4fd388d9a4c812205b3dcd5cd89d7caec7
Andrianooo <vladosandrianov@yandex.ru>
2024-11-19 15:08:29 +0300

    Сделал вторую активность

--- app/src/main/java/com/example/lab2/activity1.java
+++ app/src/main/java/com/example/lab2/activity1.java
@@ -2,7 +2,9 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity1 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn1(View view) {
+        Intent intent = new Intent(this, activity2.class);
+        startActivity(intent);
     }
 }
